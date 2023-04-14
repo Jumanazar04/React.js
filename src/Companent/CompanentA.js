072c1d597a3003676ba1ea33493d27dd42c106b2
Jumanazar04 <yusupovjumanazar004@gmail.com>
2023-04-14 08:01:26 +0500

    First

---
+++ src/Companent/CompanentA.js
@@ -0,0 +1,12 @@
+import carImg from '../assets/img.jpeg';
+
+function CompanentA() {
+    return (
+        
+            <img src={carImg} alt="img"/>
+        
+    )
+}
+
+
+export default CompanentA;
